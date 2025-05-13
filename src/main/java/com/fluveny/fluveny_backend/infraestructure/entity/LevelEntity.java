d30eac0706724c1d518ad20f656bf7e73c72de3d
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-05-13 19:48:28 -0300

    feat: adding level entity

---
+++ src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/LevelEntity.java
@@ -0,0 +1,18 @@
+package com.fluveny.fluveny_backend.infraestructure.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "fl_level")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LevelEntity {
+    private String id;
+    private String title;
+    private Integer experienceValue;
+}
