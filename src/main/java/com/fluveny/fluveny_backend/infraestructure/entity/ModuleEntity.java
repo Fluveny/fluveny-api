e665b58a070c3e1c0b592f8a7ef5f0ef0af8444b
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-05-13 20:33:14 -0300

    fix: adding id notation in grammar rule entity

--- src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/ModuleEntity.java
+++ src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/ModuleEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ModuleEntity {
+    @Id
     private String id;
     private String title;
     private String description;
