f2fa021340173c1892ff8d0708909ee1037a36f5
LBonardi23 <LBonardi2312@gmail.com>
2025-05-14 00:23:49 -0300

    feat: creating introduction mapper

---
+++ src/main/java/com/fluveny/fluveny_backend/api/mapper/IntroductionMapper.java
@@ -0,0 +1,29 @@
+package com.fluveny.fluveny_backend.api.mapper;
+
+import com.fluveny.fluveny_backend.api.dto.IntroductionDTO;
+import com.fluveny.fluveny_backend.business.service.IntroductionService;
+import com.fluveny.fluveny_backend.infraestructure.entity.IntroductionEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IntroductionMapper {
+
+    private IntroductionService introductionService;
+
+    public static IntroductionDTO toDTO(IntroductionEntity introductionEntity){
+        IntroductionDTO introductionDTO = new IntroductionDTO();
+
+        introductionDTO.setId(introductionEntity.getId());
+
+        return introductionDTO;
+    }
+
+    public static IntroductionEntity toEntity(IntroductionDTO introductionDTO){
+        IntroductionEntity introductionEntity = new IntroductionEntity();
+
+        introductionEntity.setId(introductionDTO.getId());
+
+        return introductionEntity;
+    }
+}
