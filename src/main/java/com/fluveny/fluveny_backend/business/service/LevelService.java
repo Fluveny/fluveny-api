54209f0961e91b363fc527e2562479a66e9b9a16
FelipeACorreia <lipecorreia.10@gmail.com>
2025-05-15 11:26:44 -0300

    feat: adding validation to getAllLevels method

--- src/main/java/com/fluveny/fluveny_backend/business/service/LevelService.java
+++ src/main/java/com/fluveny/fluveny_backend/business/service/LevelService.java
@@ -1,10 +1,12 @@
 package com.fluveny.fluveny_backend.business.service;
 
+import com.fluveny.fluveny_backend.exception.BusinessException.BusinessException;
 import com.fluveny.fluveny_backend.infraestructure.entity.LevelEntity;
 import com.fluveny.fluveny_backend.infraestructure.repository.LevelRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +20,11 @@ public class LevelService {
         }
 
         public List<LevelEntity> getAllLevels() {
-            return levelRepository.findAll();
+            List<LevelEntity> levels =  levelRepository.findAll();
+            if(levels.isEmpty()) {
+                throw new BusinessException("No levels found", HttpStatus.NOT_FOUND);
+            }
+            return levels;
         }
 
 }
