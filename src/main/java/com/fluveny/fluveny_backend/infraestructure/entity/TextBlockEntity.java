efef06878db8df4d58916ea58d1ce399da94993a
FelipeACorreia <lipecorreia.10@gmail.com>
2025-05-15 21:21:10 -0300

    fix: fixing wrong @Id annotation on TextBlock

--- src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/TextBlockEntity.java
+++ src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/TextBlockEntity.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "fl_textBlock")
