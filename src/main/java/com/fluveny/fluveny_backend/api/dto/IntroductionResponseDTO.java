c70466dd29c4d170b269bd5cfc10a5df58207b58
LBonardi23 <LBonardi2312@gmail.com>
2025-05-22 01:52:46 -0300

    feat: adding the introductionreposedto to create a format for introduction to be show

---
+++ src/main/java/com/fluveny/fluveny_backend/api/dto/IntroductionResponseDTO.java
@@ -0,0 +1,19 @@
+package com.fluveny.fluveny_backend.api.dto;
+
+import com.fluveny.fluveny_backend.infraestructure.entity.ModuleEntity;
+import com.fluveny.fluveny_backend.infraestructure.entity.TextBlockEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class IntroductionResponseDTO {
+    private String id;
+    private String id_module;
+    private TextBlockEntity textBlock;
+}
