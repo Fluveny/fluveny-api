e1453b034fdb9117f3c6728011fe0d49765ca2f8
LBonardi23 <LBonardi2312@gmail.com>
2025-05-22 01:51:51 -0300

    fix: refactoring introductionrepository to recieve module_id

--- src/main/java/com/fluveny/fluveny_backend/infraestructure/repository/IntroductionRepository.java
+++ src/main/java/com/fluveny/fluveny_backend/infraestructure/repository/IntroductionRepository.java
@@ -1,10 +1,14 @@
 package com.fluveny.fluveny_backend.infraestructure.repository;
 
 import com.fluveny.fluveny_backend.infraestructure.entity.IntroductionEntity;
+import com.fluveny.fluveny_backend.business.service.ModuleService;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IntroductionRepository extends MongoRepository<IntroductionEntity, String>{
-
+    List<IntroductionEntity> findAll(String modulo_id);
 }
