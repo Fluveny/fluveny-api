2748a310866a3310da3d822c4dcd58eccde07725
gustavoborinn <gustavo.borin04@gmail.com>
2025-05-28 17:33:44 -0300

    feat: adding grammar-rule controller validation

--- src/main/java/com/fluveny/fluveny_backend/api/controller/GrammarRuleController.java
+++ src/main/java/com/fluveny/fluveny_backend/api/controller/GrammarRuleController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/grammar-rules")
 @RequiredArgsConstructor
+@Validated
 public class GrammarRuleController {
 
     private final GrammarRuleService grammarRuleService;
