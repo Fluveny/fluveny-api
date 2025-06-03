a4a77925b45a46a101b0a1403c7c62717cf1173d
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-06-03 12:06:30 -0300

    feat: adding presentation service

---
+++ src/main/java/com/fluveny/fluveny_backend/business/service/PresentationService.java
@@ -0,0 +1,33 @@
+package com.fluveny.fluveny_backend.business.service;
+
+import com.fluveny.fluveny_backend.exception.BusinessException.BusinessException;
+import com.fluveny.fluveny_backend.infraestructure.entity.GrammarRuleModuleEntity;
+import com.fluveny.fluveny_backend.infraestructure.entity.PresentationEntity;
+import com.fluveny.fluveny_backend.infraestructure.repository.PresentationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PresentationService {
+    @Autowired
+    private PresentationRepository presentationRepository;
+    @Autowired
+    private GrammarRuleModuleService grammarRuleModuleService;
+
+    public PresentationEntity getPresentationById(String id) {
+        Optional<PresentationEntity> presentationEntity = presentationRepository.findById(id);
+
+        if(presentationEntity.isEmpty()) {
+            throw new BusinessException("No Presentation with this ID was found.", HttpStatus.NOT_FOUND);
+        }
+        return presentationEntity.get();
+    }
+
+    public PresentationEntity saveGrammarRuleModule(PresentationEntity presentationEntity) {
+        grammarRuleModuleService.addPresentationContent(presentationEntity.getGrammarRuleModuleId(), presentationEntity);
+        return presentationRepository.save(presentationEntity);
+    }
+}
