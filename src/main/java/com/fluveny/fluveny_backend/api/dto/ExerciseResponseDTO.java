847f205bf6c18ccfaa514a893acc7f88e555debb
LBonardi23 <LBonardi2312@gmail.com>
2025-06-04 19:55:02 -0300

    feat: creating the exercise's responsedto

---
+++ src/main/java/com/fluveny/fluveny_backend/api/dto/ExerciseResponseDTO.java
@@ -0,0 +1,18 @@
+package com.fluveny.fluveny_backend.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseResponseDTO {
+    private String grammarRuleModuleId;
+    private String header;
+    private String phrase;
+    private String template;
+    private String justification;
+}
