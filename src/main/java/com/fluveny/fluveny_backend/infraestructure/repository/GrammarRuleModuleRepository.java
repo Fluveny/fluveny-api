18280b66d84850462ddd70c626c0e7805197954b
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-06-05 10:12:20 -0300

    feat: adding new grammar rule query

--- src/main/java/com/fluveny/fluveny_backend/infraestructure/repository/GrammarRuleModuleRepository.java
+++ src/main/java/com/fluveny/fluveny_backend/infraestructure/repository/GrammarRuleModuleRepository.java
@@ -4,6 +4,9 @@ import com.fluveny.fluveny_backend.infraestructure.entity.GrammarRuleModuleEntit
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GrammarRuleModuleRepository extends MongoRepository<GrammarRuleModuleEntity, String> {
+    Optional<GrammarRuleModuleEntity> findByModuleIdAndGrammarRuleId(String moduleId, String grammarRuleId);
 }
