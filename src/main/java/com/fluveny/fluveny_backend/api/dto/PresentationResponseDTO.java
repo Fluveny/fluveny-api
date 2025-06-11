bdaa6ed2c08990b3d85bed722907fdf7003f226e
gustavoborinn <gustavo.borin04@gmail.com>
2025-06-11 14:06:43 -0300

    feat: adding presentation response DTO

---
+++ src/main/java/com/fluveny/fluveny_backend/api/dto/PresentationResponseDTO.java
@@ -0,0 +1,19 @@
+package com.fluveny.fluveny_backend.api.dto;
+
+import com.fluveny.fluveny_backend.infraestructure.entity.TextBlockEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PresentationResponseDTO {
+
+    private String id;
+    private String grammarRuleModuleId;
+    private String title;
+    private TextBlockEntity textBlock;
+}
