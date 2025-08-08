46ff46453cacbe41019ab3247256b9f88cae9ce7
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-08-07 22:15:36 -0300

    feat: adding endpoints to edit and get a presentation by id

--- src/main/java/com/fluveny/fluveny_backend/api/controller/GrammarRulePresentationController.java
+++ src/main/java/com/fluveny/fluveny_backend/api/controller/GrammarRulePresentationController.java
@@ -1,10 +1,14 @@
 package com.fluveny.fluveny_backend.api.controller;
 
 import com.fluveny.fluveny_backend.api.ApiResponseFormat;
+import com.fluveny.fluveny_backend.api.dto.ExerciseRequestDTO;
 import com.fluveny.fluveny_backend.api.dto.PresentationRequestDTO;
 import com.fluveny.fluveny_backend.api.mapper.PresentationMapper;
+import com.fluveny.fluveny_backend.api.response.exercise.ExerciseResponse;
 import com.fluveny.fluveny_backend.api.response.presentation.PresentationResponse;
+import com.fluveny.fluveny_backend.business.service.ModuleService;
 import com.fluveny.fluveny_backend.business.service.PresentationService;
+import com.fluveny.fluveny_backend.infraestructure.entity.ExerciseEntity;
 import com.fluveny.fluveny_backend.infraestructure.entity.GrammarRuleModuleEntity;
 import com.fluveny.fluveny_backend.infraestructure.entity.PresentationEntity;
 import com.fluveny.fluveny_backend.infraestructure.repository.GrammarRuleModuleRepository;
@@ -29,6 +33,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class GrammarRulePresentationController {
 
     private final PresentationService presentationService;
+    private final ModuleService moduleService;
     private final PresentationMapper presentationMapper;
     private final GrammarRuleModuleRepository grammarRuleModuleRepository;
 
@@ -79,5 +84,72 @@ public class GrammarRulePresentationController {
         PresentationEntity presentation = presentationService.createPresentation(presentationMapper.toEntity(presentationRequestDTO, id_grammarRuleModule));
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponseFormat<PresentationEntity>("Presentation created successfully", presentation));
+    }
+
+    @Operation(summary = "Return a presentation by id",
+            description = "This endpoint is used to return a presentation by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Presentation found successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PresentationResponse.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Bad request for application",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponseFormat.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "404", description = "Presentation not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponseFormat.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content  = @Content(
+                            mediaType = "application/json",
+                            schema =  @Schema(implementation = ApiResponseFormat.class)
+                    )
+            )
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponseFormat<PresentationEntity>> getExerciseByID(
+            @PathVariable String id_grammarRuleModule, @PathVariable String id_module, @PathVariable String id){
+        moduleService.grammarRuleModuleExistsInModule(id_grammarRuleModule, id_module);
+        PresentationEntity presentation = presentationService.getPresentationByIdAndValidateGrammarRuleModule(id, id_grammarRuleModule);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponseFormat<PresentationEntity>("Presentation find with successfully", presentation));
+    }
+
+    @Operation(summary = "Update a  Presentation",
+            description = "This endpoint is used to update a presentation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Presentation updated successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PresentationResponse.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Bad request for application",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponseFormat.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content  = @Content(
+                            mediaType = "application/json",
+                            schema =  @Schema(implementation = ApiResponseFormat.class)
+                    )
+            )
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponseFormat<PresentationEntity>> updatePresentation(
+            @Valid @RequestBody PresentationRequestDTO presentationRequestDTO,
+            @PathVariable String id_grammarRuleModule, @PathVariable String id_module, @PathVariable String id){
+        moduleService.grammarRuleModuleExistsInModule(id_grammarRuleModule, id_module);
+        PresentationEntity presentation = presentationService.updatePresentationAndValidateGrammarRuleModule(presentationMapper.toEntity(presentationRequestDTO, id_grammarRuleModule), id, id_module);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseFormat<PresentationEntity>("Exercise updated with successfully", presentation));
     }
 }
