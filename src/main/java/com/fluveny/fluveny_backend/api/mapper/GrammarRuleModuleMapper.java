3a03bec6e06aa159c48b9df82c27833f8e75d6d9
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-08-07 22:11:08 -0300

    feat: adding method to tiny grammar rule module DTO

--- src/main/java/com/fluveny/fluveny_backend/api/mapper/GrammarRuleModuleMapper.java
+++ src/main/java/com/fluveny/fluveny_backend/api/mapper/GrammarRuleModuleMapper.java
@@ -1,9 +1,6 @@
 package com.fluveny.fluveny_backend.api.mapper;
 
-import com.fluveny.fluveny_backend.api.dto.GrammarRuleModuleRequestDTO;
-import com.fluveny.fluveny_backend.api.dto.GrammarRuleModuleResponseDTO;
-import com.fluveny.fluveny_backend.api.dto.GrammarRuleRequestDTO;
-import com.fluveny.fluveny_backend.api.dto.GrammarRuleResponseDTO;
+import com.fluveny.fluveny_backend.api.dto.*;
 import com.fluveny.fluveny_backend.infraestructure.entity.GrammarRuleEntity;
 import com.fluveny.fluveny_backend.infraestructure.entity.GrammarRuleModuleEntity;
 import org.springframework.stereotype.Component;
@@ -22,5 +19,12 @@ public class GrammarRuleModuleMapper {
         dto.setContentList(entity.getContentList());
 
         return dto;
+    }
+
+    public GrammarRuleModuleTinyDTO toTinyDTO(GrammarRuleModuleEntity entity) {
+        GrammarRuleModuleTinyDTO tinyDto = new GrammarRuleModuleTinyDTO();
+        tinyDto.setId(entity.getId());
+        tinyDto.setTitle(entity.getGrammarRule().getTitle());
+        return tinyDto;
     }
 }
