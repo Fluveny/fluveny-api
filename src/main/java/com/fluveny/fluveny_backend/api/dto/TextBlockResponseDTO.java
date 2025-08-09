3752d130fa496676871d192e6eb52c58cd414d26
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-08-09 11:11:43 -0300

    feat: adding a text block responde DTO

---
+++ src/main/java/com/fluveny/fluveny_backend/api/dto/TextBlockResponseDTO.java
@@ -0,0 +1,14 @@
+package com.fluveny.fluveny_backend.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TextBlockResponseDTO {
+    private String content;
+}
