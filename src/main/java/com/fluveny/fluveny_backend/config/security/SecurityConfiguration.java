dfd0e222d41a4b29878098edeef2f6310fa97576
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-09-02 22:58:28 -0300

    feat: adding security configuration in the project

---
+++ src/main/java/com/fluveny/fluveny_backend/config/security/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.fluveny.fluveny_backend.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // permite todas as requisições
+        return http.build();
+    }
+}
