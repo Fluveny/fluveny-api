2f6e1cd96e185cd7218b59cbb6feda71435b0362
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-10-25 13:53:59 -0300

    fix: fixing the problem whith abstract class

--- src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/content/ContentEntity.java
+++ src/main/java/com/fluveny/fluveny_backend/infraestructure/entity/content/ContentEntity.java
@@ -1,5 +1,11 @@
 package com.fluveny.fluveny_backend.infraestructure.entity.content;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fluveny.fluveny_backend.api.dto.exercise.ExerciseCompletePhraseRequestDTO;
+import com.fluveny.fluveny_backend.api.dto.exercise.ExerciseConstructorPhraseRequestDTO;
+import com.fluveny.fluveny_backend.api.dto.exercise.ExerciseTranslateRequestDTO;
 import com.fluveny.fluveny_backend.infraestructure.enums.ContentType;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,6 +14,16 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Setter
 @Getter
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type",
+        visible = true
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ContentExerciseEntity.class, name = "EXERCISE"),
+        @JsonSubTypes.Type(value = ContentPresentationEntity.class, name = "PRESENTATION"),
+})
 public abstract class ContentEntity {
     @EqualsAndHashCode.Include
     private ContentType type;
